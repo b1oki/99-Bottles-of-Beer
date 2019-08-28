9bb9f6e7efc264591b0ff26562eb9670c4a03915
Alexander Sobyanin <alexander.sobyanin@gmail.com>
2019-08-29 02:59:22 +0500

    Add translation
    Issue #1

--- bottles.py
+++ bottles.py
@@ -1,15 +1,18 @@
+from gettext import gettext as _
+
+
 def get_bottle_word_ru(beerNum):
     if beerNum % 10 == 1 and beerNum != 11:
-        word = 'бутылка'
+        word = _('бутылка')
     elif 0 < beerNum < 5:
-        word = 'бутылки'
+        word = _('бутылки')
     else:
-        word = 'бутылок'
+        word = _('бутылок')
     return word
 
 
 for bottles in range(99, 0, -1):
-    print(bottles, get_bottle_word_ru(bottles), 'пива на стене')
-    print(bottles, get_bottle_word_ru(bottles), 'пива!')
-    print('Возьми одну, пусти по кругу')
-print('Нет бутылок пива на стене')
+    print(bottles, get_bottle_word_ru(bottles), _('пива на стене'))
+    print(bottles, get_bottle_word_ru(bottles), _('пива!'))
+    print(_('Возьми одну, пусти по кругу'))
+print(_('Нет бутылок пива на стене'))
