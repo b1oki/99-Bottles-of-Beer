789744c20be22ce634999e3fc68146ad5abd7f74
Alexander Sobyanin <alexander.sobyanin@gmail.com>
2019-09-05 23:53:22 +0500

    Add setuptools

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='99-Bottles-of-Beer',
+    version='1.0.0',
+    packages=find_packages(),
+    url='https://github.com/b1oki/99-Bottles-of-Beer',
+    license='GNU General Public License v3.0',
+    author='b1oki',
+    author_email='git@alexandersobyanin.ru',
+    description='99 Bottles of Beer Song',
+)
